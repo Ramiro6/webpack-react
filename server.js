652e0b0a5a40bdb367e498f87f3ca361def0cd4f
ramiro6 <ramiroruiz578@gmail.com>
2018-06-26 11:38:24 +0200

    add basic server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('Init server');
+const express = require('express')
+const app = express()
+
+
+
+
+
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+  console.log(`Init new server on Port ${port}`)
+})
