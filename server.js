60ae04b66e2d58cc4466809ae829333c7d2c0bf7
ramiro6 <ramiroruiz578@gmail.com>
2018-06-27 16:57:44 +0200

    add server static

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
 
-
-
-
 const port = process.env.PORT || 9000
+
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(port, () => {
   console.log(`Init new server on Port ${port}`)
